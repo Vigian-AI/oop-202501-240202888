859b8b61b254d08642fbc36eb954d8c3237d6d9c
Fendy134 <agustianfendy6@gmail.com>
2025-10-10 21:27:05 +0700

    Add files via upload

---
+++ praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProcedural.java
@@ -0,0 +1,8 @@
+// HelloProcedural
+public class HelloProcedural {
+    public static void main(String[] args) {
+        String nim = "240202898";
+        String name = "Fendy Agustian";
+        System.out.println("Hello World, I am " + name + " - " + nim);
+    }
+}
