74c88d5f204e6dea3bf75d4549b9c3cdb637e9a5
Fendy134 <agustianfendy6@gmail.com>
2025-10-28 22:21:23 +0700

    Delete oop-240202898 directory

--- oop-240202898/praktikum/week1-setup-hello-pos/src/main/java/com/upb/agripos/HelloProcedural.java
+++
@@ -1,8 +0,0 @@
-// HelloProcedural
-public class HelloProcedural {
-    public static void main(String[] args) {
-        String nim = "240202898";
-        String name = "Fendy Agustian";
-        System.out.println("Hello World, I am " + name + " - " + nim);
-    }
-}
